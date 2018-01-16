2d5ff353fc3fb5552d8d1433b594660d1ead7d90
ZL <502857421@qq.com>
2018-01-16 18:20:31 +0800

    登陆界面整体设计

---
+++ web/js/login.js
@@ -0,0 +1,22 @@
+/**
+ * 初始页面隐藏重置密码模块
+ */
+$(document).ready(function(){
+   $("#resetModule").hide();
+});
+
+/**
+ * 隐藏登陆模块，显示重置密码模块
+ */
+function reset() {
+    $("#loginModule").hide(500);
+    $("#resetModule").show(1000);
+}
+
+/**
+ * 隐藏重置密码模块，显示登陆模块
+ */
+function login(){
+    $("#resetModule").hide(500);
+    $("#loginModule").show(1000);
+}
